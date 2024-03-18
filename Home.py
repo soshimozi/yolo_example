2081acf9764901051118a89f254c016a62ca5d78
Soshimozi <scott.mccain@gmail.com>
2024-03-17 22:05:37 -0700

    First commit

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+
+st.set_page_config(page_title="Home",
+                   layout='wide',
+                   page_icon='./images/home.png')
+
+st.title("YOLO V5 Object Detection App")
+st.caption('This web application demonstrates Object Detection')
+
+st.sidebar.success("Select a demo above.")
+
+# Content
